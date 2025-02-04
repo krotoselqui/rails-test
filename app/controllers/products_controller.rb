9b8eec6760359a1dc8511b144764d37f415e7fa2
krotoselqui <dragon0549610@yahoo.co.jp>
2025-02-04 11:44:46 +0900

    added error message on new product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,7 +22,7 @@ class ProductsController < ApplicationController
 
   private
     def product_params
-      params.expect(product: [ :name ])
+      params.require(:product).permit(:name)
     end
 
 end
